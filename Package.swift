025c112d0ccc0463380a2e792552f17aa206c3c8
Johan Rydenstam <johan.rydenstam@kebne.se>
2022-11-10 15:20:43 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftDraw",
 	  platforms: [
-        .iOS(.v10), .macOS(.v10_12)
+        .iOS(.v10), .macOS(.v10_12), .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
